4500b5ac39d0dfeb6f71bd882a73020478cba820
danse <f.occhipinti@gmail.com>
2016-05-27 18:01:25 +0200

    :boom:

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,66 +2,46 @@
 {-# LANGUAGE RecordWildCards #-}
 module Lib where
 
+import Web.Authenticate.OAuth
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS( tlsManagerSettings )
-import Network.OAuth
-import Network.OAuth.ThreeLegged
-import Network.OAuth.Types.Params( workflow, Workflow(TemporaryTokenRequest) )
-import Crypto.Random
-import Network.URI
-import Data.ByteString( getLine )
-import Data.ByteString.Lazy( putStrLn )
-import Data.Functor( fmap )
+import qualified Data.ByteString.Lazy.Char8 as C
 import Data.Maybe( fromJust )
-import Network.HTTP.Client.Internal ( dummyConnection, requestBuilder )
+import Data.String( fromString )
+import Control.Monad.IO.Class( MonadIO )
 
-base = "https://www.upwork.com/api"
-authBase = base ++ "/auth/v1/oauth"
-jobsLoc = base ++ "/profiles/v2/search/jobs.json"
+base = "https://www.upwork.com"
+authBase = base ++ "/api/auth/v1/oauth/token"
+jobsLoc = base ++ "/api/profiles/v2/search/jobs.json"
 
-maybeGetJobs :: Maybe Request
-maybeGetJobs = parseUrl jobsLoc
-
--- http://hackage.haskell.org/package/oauthenticated-0.1.3.4/docs/Network-OAuth-ThreeLegged.html
--- https://developers.upwork.com/?lang=python#authentication_oauth-10
-
-threeLegged = fromJust (parseThreeLegged r1 r2 r3 OutOfBand)
-  where
-    r1 = authBase ++ "/token/request"
-    r2 = "https://www.upwork.com/services/api/auth"
-    r3 = authBase ++ "/token/access"
-
-getVerifier :: URI -> IO Verifier
-getVerifier uri = do
-  Prelude.putStrLn $ "please go to " ++ (show uri)
-  Prelude.putStrLn $ "after accepting, paste the verifier code below and hit enter"
-  Data.ByteString.getLine
-
-server = defaultServer { parameterMethod=RequestEntityBody }
-
-requestToken threeLegged key secret =
-  let client = clientCred (Token key secret)
-  in requestTokenProtocol' tlsManagerSettings client server threeLegged getVerifier
+getJobs :: Request
+getJobs = fromJust (parseUrl jobsLoc)
 
 queryAll = setQueryString [("q", Just "*")]
 
-getTemporaryTokenRequest key secret = do
-  entropy <- createEntropyPool
-  (oax, gen') <- freshOa (clientCred (Token key secret)) (cprgCreate entropy :: SystemRNG)
-  return (sign (oax { workflow = TemporaryTokenRequest (callback threeLegged) }) server (temporaryTokenRequest threeLegged))
+-- https://developers.upwork.com/?lang=python#authentication_oauth-10
+makeOAuth key secret = 
+  def {
+    oauthRequestUri = authBase ++ "/request",
+    oauthAccessTokenUri = authBase ++ "/access",
+    oauthAuthorizeUri = base ++ "/services/api/auth",
+    oauthConsumerKey = key,
+    oauthConsumerSecret = secret
+    }
 
-askForJobs key secret = do
-  maybePermanent <- requestToken threeLegged key secret
-  case maybePermanent of
-    Nothing -> Prelude.putStrLn "Nothing permanent"
-    Just permanent -> do
-      entropy <- createEntropyPool
-      (signed, gen) <- oauth permanent server (queryAll (fromJust maybeGetJobs)) (cprgCreate entropy :: SystemRNG)
-      manager <- newManager defaultManagerSettings
-      response <- httpLbs signed manager
-      Data.ByteString.Lazy.putStrLn (responseBody response)
+getCredentials :: OAuth -> IO Credential
+getCredentials oauth = do
+  manager <- newManager tlsManagerSettings
+  temporaryCredential <- getTemporaryCredential oauth manager
+  putStrLn "please go to:"
+  putStrLn (authorizeUrl oauth temporaryCredential)
+  putStrLn "after accepting, paste the verifier code below and hit enter"
+  verifier <- getLine
+  putStrLn $ "you pasted `"++ verifier ++"`"
+  getAccessToken oauth (injectVerifier (fromString verifier) temporaryCredential) manager
 
-printRequestBody (RequestBodyLBS byteString) = byteString
-printRequestBody _ = "other constructor"
-
-
+askForJobs oauth tokenCredentials = do
+  signed <- signOAuth oauth tokenCredentials (queryAll getJobs)
+  manager <- newManager tlsManagerSettings
+  response <- httpLbs signed manager
+  C.putStrLn (responseBody response)
