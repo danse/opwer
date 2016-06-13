df9bd4b174b9dfd4f4072499316f1a0c3f25b6b5
danse <f.occhipinti@gmail.com>
2016-06-13 16:28:36 +0200

    read the search query from the file system

--- app/opwer-search.hs
+++ app/opwer-search.hs
@@ -1,7 +1,21 @@
 import Opwer
 import Upwork
 import Web.Authenticate.OAuth( Credential )
+import Data.ByteString( ByteString )
+import Data.String( fromString )
+import Data.Maybe( Maybe( Just ) )
+import System.Environment( getArgs )
+
+readQuery :: String -> [(String, String)]
+readQuery = read
+
+adaptQuery :: (String, String) -> (ByteString, Maybe ByteString)
+adaptQuery (a,b) = (fromString a, (Just . fromString) b)
+
+convertQuery = (map adaptQuery) . readQuery
 
 main = do
+  [arg] <- getArgs
+  queryContents <- readFile arg
   OpwerCredential oauth credential <- readCredential
-  askForJobs oauth credential
+  askForJobs oauth credential (convertQuery queryContents)
