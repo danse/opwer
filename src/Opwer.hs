c4d3015eb658be1d009b20cd461406f1c5b5c4a7
danse <f.occhipinti@gmail.com>
2016-06-13 16:02:19 +0200

    add sorting utilities

--- src/Opwer.hs
+++ src/Opwer.hs
@@ -2,6 +2,11 @@
 module Opwer where
 
 import Web.Authenticate.OAuth
+import qualified Data.ByteString.Lazy.Char8 as C
+import Data.Aeson( eitherDecode )
+import Control.Monad( mapM )
+import Data.Either( rights, lefts )
+import Upwork
 
 credentialFileName = "opwer-credential"
 
@@ -15,3 +20,15 @@ readCredential = do
   s <- readFile credentialFileName
   return (read s)
 
+sortContents :: C.ByteString -> ([JobProfile] -> [JobProfile]) -> [String]
+sortContents cont sort_ = (((map show) . sort_ . (map profile)) r) ++ l
+  where r = rights decoded
+        l = lefts decoded
+        decoded = map eitherDecode (C.lines cont)
+
+-- |Given a sorting function, reads lines with job profiles from
+-- standard input, and outputs them sorted
+sortJobDetails :: ([JobProfile] -> [JobProfile]) -> IO ([()])
+sortJobDetails fun = do
+  contents <- C.getContents
+  mapM putStrLn (sortContents contents fun)
