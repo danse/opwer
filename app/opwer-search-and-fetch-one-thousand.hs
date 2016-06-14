9fdf188e19bcfa34b2228528436db9fe30677ead
danse <f.occhipinti@gmail.com>
2016-06-14 21:13:00 +0200

    add a command to get one thousand search results

---
+++ app/opwer-search-and-fetch-one-thousand.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE PartialTypeSignatures #-}
+import Opwer
+import Upwork
+import Web.Authenticate.OAuth( Credential )
+import Data.ByteString( ByteString )
+import Data.String( fromString )
+import Data.Maybe( Maybe( Just ) )
+import System.Environment( getArgs )
+import Control.Exception( catch )
+import Data.Aeson( decode )
+import Network.HTTP.Client( responseBody )
+import Control.Applicative( (<$>) )
+import Control.Concurrent( forkIO )
+
+
+readQuery :: String -> [(String, String)]
+readQuery = read
+
+adaptQuery :: (String, String) -> (ByteString, Maybe ByteString)
+adaptQuery (a,b) = (fromString a, (Just . fromString) b)
+
+addPages :: String -> [[(String, String)]]
+addPages queryContents = map addToQuery pages
+  where addToQuery page = ("paging", (show (page*100))++";100") : query
+        pages = [0..9]
+        query = readQuery queryContents
+
+askAndPrintParsed :: _ -> _ -> _ -> IO ()
+askAndPrintParsed oauth credential id = do
+  resp <- askForJob oauth credential id
+  putStrLn ((parseJobDetails . responseBody) resp)
+
+searchAndFetch oauth credential query = do
+  resp <- askForJobs oauth credential (map adaptQuery query)
+  case (decode (responseBody resp) :: Maybe SearchResult) of
+    Nothing -> putStrLn "The input does not seem valid"
+    Just result -> do
+      mapM (\ (JobResult id) -> catch (askAndPrintParsed oauth credential id) printException) (jobs result)
+      return ()
+
+main = do
+  [arg] <- getArgs
+  queryContents <- readFile arg
+  OpwerCredential oauth credential <- readCredential
+  sequence (map (searchAndFetch oauth credential) (addPages queryContents))
+  return ()
