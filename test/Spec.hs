81e078bf24c11b296454790bf8a56e25254c1b00
danse <f.occhipinti@gmail.com>
2016-10-14 00:12:24 +0200

    add some notes about test ideas

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,25 @@
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
+
+{-
+
+#### Some test ideas
+
+let p = number of proposals
+    i = interest ratio
+    r = probability of getting attention
+
+if the client interviews only one applicant, let's say that the
+probability of getting attention is inversely proportional to the
+number of proposals:
+
+    r = 1 / p
+
+but if the client interviews everyone who sends a proposal, the
+probability of getting attention is 1
+
+with other parameters staying constant, the probability of getting
+attention is equal to the number of interviewed applicants versus all
+applicants, which is how i calculate the interest rate
+
+-}
